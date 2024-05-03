7bca11801d3c797f8e908a3bdb0b607fdce2588c
Jecília Teixeira <111911408+Jecilia@users.noreply.github.com>
2024-05-03 23:55:12 +0100

    ...

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import logo from './assets/logo.svg'
+export function App() {
+  return (
+    <div>
+      <button type="button">
+        <img src={logo} alt="rocketseat" />
+      </button>
+      <h1 className="text-zinc-50">jecilia</h1>
+    </div>
+  )
+}
